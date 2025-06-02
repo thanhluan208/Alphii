3ffc099eaf617184c5c59188289e1f6cd973abbd
duydn2 <dongocduy.duydo@gmail.com>
2025-06-02 20:31:10 +0700

    feat: update code for login

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,6 +14,7 @@ import { notFound } from "next/navigation"
 import { routing } from "@/i18n/routing"
 import { ThemeProvider } from "@/providers/themeProvider"
 import { twJoin } from "tailwind-merge"
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -54,6 +55,7 @@ export default async function RootLayout({
 						</Providers>
 					</ThemeProvider>
 				</NextIntlClientProvider>
+				<Toaster />
 			</body>
 		</html>
 	)
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,11 +1,18 @@
 import { persist } from "zustand/middleware"
 import { shallow } from "zustand/shallow"
 import { createWithEqualityFn } from "zustand/traditional"
-import { Profile } from "@/types/profile.type"
+import { LOCAL_STORAGE_KEY } from "@/lib/constant";
+import { Profile } from "@/types/user.type";
 
 interface userState {
-	Profile: Profile | null
-	setProfileData: (value: Profile | null) => void
+	Profile: Profile | null;
+	setProfileData: (value: Profile | null) => void;
+	token: string | null;
+	setToken: (value: string | null) => void;
+	language: string;
+	setLanguage: (value: string) => void;
+	userId: string | null,
+	setUserId: (value: string | null) => void,
 }
 
 const useUserStore = createWithEqualityFn<userState>()(
@@ -18,10 +25,19 @@ const useUserStore = createWithEqualityFn<userState>()(
 				set({
 					Profile: value
 				})
-			}
+			},
+			token: localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN) as string || null,
+			setToken: (value) => set({ token: value }),
+			language: "en",
+			setLanguage: (value) => set({ language: value }),
+			userId: localStorage.getItem(LOCAL_STORAGE_KEY.USER_ID) as string || null,
+			setUserId: (value) => set({ userId: value }),
 		}),
 		{
-			name: "user-store"
+			name: "user-store",
+			partialize: (state) => ({
+				language: state.language,
+			}),
 		}
 	),
 	shallow
